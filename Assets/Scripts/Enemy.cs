2cf66525906e3370583edf2dee9dec951f5b6c67
Kristians Visockis <Sylvyrrock@gmail.com>
2023-04-28 03:14:48 +0300

    Create map and enemy
    - Create basic map layout
    - Add waypoint system
    - Add enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wayPointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.waypoints[0]; 
+    }
+
+    void Update()
+    {
+        Vector2 dirrection = target.position - transform.position;
+        transform.Translate(dirrection.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector2.Distance(transform.position, target.position) <=0.5f )
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wayPointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wayPointIndex++;
+        target = Waypoints.waypoints[wayPointIndex];
+    }
+}
