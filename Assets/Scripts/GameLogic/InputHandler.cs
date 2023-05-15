5c24aded7aa21eadeb20b1ac45581e7688c40ac2
Kristians Visockis <Sylvyrrock@gmail.com>
2023-05-15 06:09:05 +0300

    Camera boundaries
    - Add camera boundaries using clamping

--- Assets/Scripts/GameLogic/InputHandler.cs
+++ Assets/Scripts/GameLogic/InputHandler.cs
@@ -6,14 +6,20 @@ public class InputHandler : MonoBehaviour
 {
     [Header("Screen movement:")]
     [SerializeField] private float speed = 50f;
-    //[SerializeField] private float padding = 10f;
-    //[SerializeField] private bool windowInFocus = true;
+    [SerializeField] private float padding = 10f;
 
     [Header("Zoom controls:")]
     [SerializeField] private float zoomFactor = 50f;
     [SerializeField] private float zoomLerpSpeed = 10f;
+
     private float targetZoom;
-    private Camera cam; 
+    private Camera cam;
+
+    [Header ("Camera boundaries:")]
+    [SerializeField] private float leftLimit = -50f;
+    [SerializeField] private float rightLimit = 130f;
+    [SerializeField] private float topLimit = 135f;
+    [SerializeField] private float bottomLimit = -60f;
 
     private void Start()
     {
@@ -29,46 +35,38 @@ public class InputHandler : MonoBehaviour
             return;
         }
 
-        // Refactor or remove later (Constraining inputs when coursore is out of bound)
-        //Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
-        //if (!screenRect.Contains(Input.mousePosition))
-        //    return;
-        
-        // Escape menu
-        //if (Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    windowInFocus = !windowInFocus;
-        //    // TODO: Escape menu
-        //}
 
-        //if (!windowInFocus)
-        //{
-        //    return;
-        //}
+        // Limit camera movement to set boundaries
+        transform.position = new Vector3
+            (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit), 
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit), 
+            transform.position.z
+            );
 
         // Zoom logic
         float zoomAxis = Input.GetAxis("Mouse ScrollWheel");
         targetZoom -= zoomAxis * zoomFactor;
-        targetZoom = Mathf.Clamp(targetZoom, 10, 50);
+        targetZoom = Mathf.Clamp(targetZoom, 10, 45);
         cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoom, ref zoomAxis, Time.deltaTime * zoomLerpSpeed);
 
-        // Camera movement logic                                         Add code for mouse movement later.
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) // || Input.mousePosition.y >= Screen.height - padding
+        // Camera movement logic
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - padding) //|| Input.mousePosition.y >= Screen.height - padding
         {
             transform.Translate(Vector2.up * Time.deltaTime * speed, Space.World);
         }
 
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) // || Input.mousePosition.y <= padding
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= padding) //|| Input.mousePosition.y <= padding
         {
             transform.Translate(-Vector2.up * Time.deltaTime * speed, Space.World);
         }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // || Input.mousePosition.x <= padding
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= padding) //|| Input.mousePosition.x <= padding
         {
             transform.Translate(Vector2.left * Time.deltaTime * speed, Space.World);
         }
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // || Input.mousePosition.x >= Screen.width - padding
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - padding) //|| Input.mousePosition.x >= Screen.width - padding
         {
             transform.Translate(-Vector2.left * Time.deltaTime * speed, Space.World);
         }
